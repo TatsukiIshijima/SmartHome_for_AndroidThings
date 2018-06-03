43d53dd710706d33532db1501a64ed7141e598f7
TatsukiIshijima <tatsuki445@gmail.com>
2018-06-03 19:40:58 +0900

    FirebaseでDBを監視してGPIO制御するよう追記

--- app/src/main/java/com/io/tatsuki/smarthome/MainActivity.kt
+++ app/src/main/java/com/io/tatsuki/smarthome/MainActivity.kt
@@ -3,34 +3,21 @@ package com.io.tatsuki.smarthome
 import android.app.Activity
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.PeripheralManager
+import com.google.firebase.database.*
 import java.io.IOException
 
-/**
- * Skeleton of an Android Things activity.
- *
- * Android Things peripheral APIs are accessible through the class
- * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
- * set it to HIGH:
- *
- * <pre>{@code
- * val service = PeripheralManagerService()
- * val mLedGpio = service.openGpio("BCM6")
- * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
- * mLedGpio.value = true
- * }</pre>
- * <p>
- * For more complex peripherals, look for an existing user-space driver, or implement one if none
- * is available.
- *
- * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
- *
- */
 class MainActivity : Activity() {
 
     private val TAG = MainActivity::class.simpleName
+
+    enum class SwitchList() {
+        TV
+    }
 
     private val SWITCH_1 = "BCM4"
     private val SWITCH_2 = "BCM17"
@@ -43,7 +30,9 @@ class MainActivity : Activity() {
     private val SWITCH_9 = "BCM22"
     private val SWITCH_10 = "BCM23"
 
+    private var mFristFlg = true;
     private var tvGPIO: Gpio? = null
+    private val mTVDatabaseRef = FirebaseDatabase.getInstance().reference.child(SwitchList.TV.toString())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,11 +43,11 @@ class MainActivity : Activity() {
         Log.d(TAG, "PRODUCT : " + Build.PRODUCT)
 
         init()
+        readData()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
         try {
             // GPIOの使用を終了する
             tvGPIO?.close()
@@ -78,9 +67,44 @@ class MainActivity : Activity() {
             tvGPIO = peripheralManager.openGpio(SWITCH_1)
             // 出力モード：初期値0V
             tvGPIO?.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
-            //tvGPIO?.value = true
         } catch (e: IOException) {
             Log.e(TAG, "error", e)
         }
+    }
+
+    /**
+     * データの書き込み
+     * @type        機器の種類
+     * @isLaunch    起動中か
+     */
+    private fun writeData(type: String, isLaunch: Boolean) {
+        when(type) {
+            SwitchList.TV.toString() -> {
+                // DB書き込み
+                mTVDatabaseRef.setValue(isLaunch)
+            }
+        }
+    }
+
+    private fun readData() {
+        mTVDatabaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e(TAG, "error", databaseError.toException())
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                Log.d(TAG, "TV value : " + dataSnapshot.value)
+                // アプリ起動時にも呼ばれるため初回のみ何もしないようにする
+                if (mFristFlg) {
+                    mFristFlg = false
+                } else {
+                    // 基本は赤外線を送信するだけなので使用後はDBの値に限らずGPIOをfalseに戻す
+                    tvGPIO?.value = true
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        tvGPIO?.value = false
+                    }, 2000)
+                }
+            }
+        })
     }
 }
