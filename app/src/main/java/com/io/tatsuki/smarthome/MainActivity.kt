8edd9e862c8784509d9502e846f8868ac5bf4592
TatsukiIshijima <tatsuki445@gmail.com>
2018-06-03 14:38:18 +0900

    GPIOで出力できるように追記

--- app/src/main/java/com/io/tatsuki/smarthome/MainActivity.kt
+++ app/src/main/java/com/io/tatsuki/smarthome/MainActivity.kt
@@ -4,7 +4,9 @@ import android.app.Activity
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.PeripheralManager
+import java.io.IOException
 
 /**
  * Skeleton of an Android Things activity.
@@ -30,6 +32,19 @@ class MainActivity : Activity() {
 
     private val TAG = MainActivity::class.simpleName
 
+    private val SWITCH_1 = "BCM4"
+    private val SWITCH_2 = "BCM17"
+    private val SWITCH_3 = "BCM27"
+    private val SWITCH_4 = "BCM18"
+    private val SWITCH_5 = "BCM5"
+    private val SWITCH_6 = "BCM6"
+    private val SWITCH_7 = "BCM13"
+    private val SWITCH_8 = "BCM12"
+    private val SWITCH_9 = "BCM22"
+    private val SWITCH_10 = "BCM23"
+
+    private var tvGPIO: Gpio? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate")
@@ -38,9 +53,34 @@ class MainActivity : Activity() {
         Log.d(TAG, "BRAND : " + Build.BRAND)
         Log.d(TAG, "PRODUCT : " + Build.PRODUCT)
 
+        init()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        try {
+            // GPIOの使用を終了する
+            tvGPIO?.close()
+        } catch (e: IOException) {
+            Log.e(TAG, "error", e)
+        }
+    }
+
+    private fun init() {
         val peripheralManager = PeripheralManager.getInstance()
         Log.d(TAG, "GPIO : " + peripheralManager.gpioList.toString())
         Log.d(TAG, "PWM : " + peripheralManager.pwmList.toString())
         Log.d(TAG, "I2C : " + peripheralManager.i2cBusList.toString())
+
+        try {
+            // 使用するGPIOの指定
+            tvGPIO = peripheralManager.openGpio(SWITCH_1)
+            // 出力モード：初期値0V
+            tvGPIO?.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+            //tvGPIO?.value = true
+        } catch (e: IOException) {
+            Log.e(TAG, "error", e)
+        }
     }
 }
